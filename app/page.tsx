f4b37274fc16994d3d94b7261db7588cff740adb
Tyler Pohl <tylercurtispohl@gmail.com>
2023-07-07 11:18:44 -0700

    call hello API function

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,51 @@
 "use client";
 import Image from "next/image";
 import "@aws-amplify/ui-react/styles.css";
-import { Amplify } from "aws-amplify";
+import { Amplify, API, Auth } from "aws-amplify";
 import awsExports from "../src/aws-exports";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from "../src/aws-exports";
+import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
 
 Amplify.configure({ ...awsExports, ssr: true });
+Auth.configure(awsconfig);
 
-export default withAuthenticator(function Home({ signOut, user }) {
+const withAuthenticatorOptions = {
+  hideSignUp: true,
+};
+
+export default withAuthenticator(function Home({
+  signOut,
+  user,
+}: {
+  signOut: ((data?: AuthEventData | undefined) => void) | undefined;
+  user: AmplifyUser | undefined;
+}) {
+  async function callApi() {
+    const authenticatedUser = await Auth.currentAuthenticatedUser();
+    const token = authenticatedUser.signInUserSession.idToken.jwtToken;
+    console.log("token: ", token);
+
+    const requestData = {
+      headers: {
+        Authorization: token,
+      },
+    };
+    const data = await API.get("helloapi", "/hello", requestData);
+    console.log("data: ", data);
+  }
+
   return (
     <main>
       <div>Home</div>
       <h1>Logged in as {user?.username}.</h1>
       <div>
+        <button onClick={callApi}>Call API</button>
+      </div>
+      <div>
         <button onClick={signOut}>Sign out</button>
       </div>
     </main>
   );
-});
+},
+withAuthenticatorOptions);
